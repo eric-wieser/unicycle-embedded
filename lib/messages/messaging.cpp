05921956122437551be3226ff42cc95fa8978e6b
Eric Wieser <wieser.eric@gmail.com>
2017-01-04 18:49:45 +0000

    Split messaging into header and implementation.
    Add read callbacks

---
+++ lib/messages/messaging.cpp
@@ -0,0 +1,140 @@
+#include <wiring.h>          // workaround for https://github.com/chipKIT32/chipKIT-core/pull/324
+#include <Board_Defs.h>
+#include <HardwareSerial.h>  // for Serial
+
+#include <cobs/Print.h>
+#include <cobs/Stream.h>
+#include <PacketListener.h>
+#include <pb_arduino.h>  // for as_pb_ostream
+
+#include <messaging.h>
+
+
+namespace {
+    //! our cobs packetizer
+    packetio::COBSPrint cobs_out(Serial);
+    packetio::COBSStream cobs_in(Serial);
+
+    packetio::PacketListener listener(cobs_in);
+
+    //! simple wrapper type to pass array length into write_array
+    template <typename T>
+    struct array_handle {
+        T* ptr;
+        size_t len;
+    };
+
+    //! nanopb callback for writing a string
+    bool write_string(pb_ostream_t *stream, const pb_field_t *field, void * const *arg)
+    {
+        auto argval = *arg;
+        auto handle = *reinterpret_cast<array_handle<const uint8_t>*>(argval);
+
+        if (!pb_encode_tag_for_field(stream, field))
+            return false;
+
+        return pb_encode_string(stream, handle.ptr, handle.len);
+    }
+
+    //! nanopb callback for writing an array
+    template<typename T, const pb_field_t* fields>
+    bool write_array(pb_ostream_t * stream, const pb_field_t *field, void * const *arg)
+    {
+        auto argval = *arg;
+        auto handle = *reinterpret_cast<array_handle<T>*>(argval);
+
+        for(size_t i = 0; i < handle.len; i++) {
+            if (!pb_encode_tag_for_field(stream, field))
+                return false;
+            T* curr = handle.ptr + i;
+            if (!pb_encode_submessage(stream, fields, curr))
+                return false;
+        }
+        return true;
+    }
+
+    //! Send a message object over serial, using protobuf and cobs
+    void sendMessage(RobotMessage& message) {
+        // Create stream
+        pb_ostream_t pb_stream = as_pb_ostream(cobs_out);
+
+        // serialize the message
+        bool status = pb_encode(&pb_stream, RobotMessage_fields, &message);
+
+        /* Then just check for any errors.. */
+        if (!status)
+        {
+            printf("Encoding failed: %s\n", PB_GET_ERROR(&pb_stream));
+            cobs_out.abort();
+        }
+        else{
+            // finish the packet
+            cobs_out.end();
+        }
+    }
+
+    //! base packet listener
+    void handlePacket(uint8_t* data, size_t n) {
+        pb_istream_t pb_stream = pb_istream_from_buffer(data, n);
+        debug("Got a message");
+        debug(reinterpret_cast<char*>(data));
+
+        PCMessage message = PCMessage_init_zero;
+        bool status = pb_decode(&pb_stream, PCMessage_fields, &message);
+
+        if(!status) {
+            return;
+        }
+
+        switch(message.which_msg) {
+            case PCMessage_controller_tag: messageHandlers<SetController>::handler.operator ()(message.msg.controller); return;
+            case PCMessage_go_tag:         messageHandlers<Go>::handler.operator ()(message.msg.go); return;
+            case PCMessage_stop_tag:       messageHandlers<Stop>::handler.operator ()(message.msg.stop); return;
+        }
+    }
+
+}
+
+//! do any setup required for messaging
+void setupMessaging() {
+    Serial.begin(57600);
+    listener.onMessage(handlePacket);
+}
+
+void updateMessaging() {
+    listener.update();
+}
+
+//! send a debug string
+void debug(const char* text) {
+    debug(text, strlen(text));
+}
+void debug(const char* text, size_t n) {
+    array_handle<const char> arr = {text, n};
+
+    // fill out the message
+    RobotMessage message = RobotMessage_init_zero;
+    message.which_msg = RobotMessage_debug_tag;
+    message.msg.debug.s.funcs.encode = write_string;
+    message.msg.debug.s.arg = &arr;
+
+    sendMessage(message);
+}
+
+//! send log messages
+void sendLogs(const LogEntry* entries, size_t n) {
+    array_handle<const LogEntry> arr = {entries, n};
+
+    // fill out the message
+    RobotMessage message = RobotMessage_init_zero;
+    message.which_msg = RobotMessage_log_tag;
+    message.msg.log.entries.funcs.encode = &write_array<const LogEntry, LogEntry_fields>;
+    message.msg.log.entries.arg = &arr;
+
+    sendMessage(message);
+}
+
+template <typename T> typename messageHandlers<T>::type messageHandlers<T>::handler;
+template messageHandlers<Go>::type messageHandlers<Go>::handler;
+template messageHandlers<Stop>::type messageHandlers<Stop>::handler;
+template messageHandlers<SetController>::type messageHandlers<SetController>::handler;
