8783a921a101034c378acaf98fe6b2e081d1bbbc
Eric Wieser <wieser.eric@gmail.com>
2017-04-12 21:26:22 +0100

    DOC: Add documentation of pin assignments

--- src/pins.h
+++ src/pins.h
@@ -1,29 +1,35 @@
 #pragma once
 
 
-/* These are mostly for documentation, and so we know which
-pins are already in use
+/*
+ * These are mostly for documentation, and so we know which
+ * pins are already in use.
 */
-namespace pins {
-	// encoders
+namespace pins
+{
+	/** @name Encoders */
 	static const uint8_t TT_DIR = 39;
 	static const uint8_t W_DIR = 47;
-	static const uint8_t TMR3_CLK = 22;
-	static const uint8_t TMR4_CLK = 23;
+	static const uint8_t TT_CLK = 22;
+	static const uint8_t W_CLK = 23;
+	/** @} */
 
-	// sensor(s)
+	/** @name I2C Sensors */
 	static const uint8_t I2C1_SDA = 20;
 	static const uint8_t I2C1_SCL = 21;
+	/** @} */
 
-	// motors
-	static const uint8_t OC1_PWM = 3;
-	static const uint8_t OC2_PWM = 5;
-	static const uint8_t OC3_PWM = 6;
-	static const uint8_t OC4_PWM = 9;
+	/** @name Motors */
+	static const uint8_t TT_FWD = 3;
+	static const uint8_t TT_REV = 5;
+	static const uint8_t W_FWD = 6;
+	static const uint8_t W_REV = 9;
+	/** @} */
 
-	// human interaction
-	static const uint8_t SWITCH = 43;
+	/** @name Human interaction */
+	static const uint8_t SWITCH = 43; //!< Input for the limit switch
 	static const uint8_t LED    = PIN_LED1;
 	static const uint8_t USB_RX = 0;
 	static const uint8_t USB_TX = 1;
+	/** @} */
 }
