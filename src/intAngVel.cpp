4301fac32aa7505c94b57502ad4a5b45a3566b8c
Eric Wieser <wieser.eric@gmail.com>
2017-05-17 23:59:34 +0100

    MAINT: Make it obvious where the quaternion integration is happening
    Clearly we have two methods, and it's not clear when each is appropriate

--- src/intAngVel.cpp
+++ src/intAngVel.cpp
@@ -11,36 +11,43 @@
 
 using namespace geometry;
 
+quat integrate_quat_fast(const quat &q0, const Vector3<float> &w, float dt) {
+  return (1 + 0.5*quat(w)*dt) * q0;
+}
+
+quat integrate_quat_aleksi(const quat &q0, const Vector3<float> &w, float dt) {
+  // This was previously used for a large timesteps. It is not clear where it
+  // came from. It seems to be the above with an extra high-order term
+  quat p(1 + (w[0]*dt/2.0)*(w[1]*dt/2.0)*(w[2]*dt/2.0),
+
+         (w[0]*dt/2.0) - (w[1]*dt/2.0)*(w[2]*dt/2.0),
+         (w[1]*dt/2.0) + (w[2]*dt/2.0)*(w[0]*dt/2.0),
+         (w[2]*dt/2.0) - (w[0]*dt/2.0)*(w[1]*dt/2.0));
+  return p * q0;
+}
+
 void intAngVel(quat& q,
                Vector3<float> &w0,
                const Vector3<float> &w,
                euler_angle &orient,
                euler_angle &dorient)
 {
-  Vector3<float> w_mean = (w + w0) / 2.0;
 
-  //Here below is the conversion from local angular velocities into a position quaternion.
-  // Here we have the area under the speed curve - the position!
-  quat p(1 + (w_mean[0]*dt/2.0)*(w_mean[1]*dt/2.0)*(w_mean[2]*dt/2.0),
-         (w_mean[0]*dt/2.0) - (w_mean[1]*dt/2.0)*(w_mean[2]*dt/2.0),
-         (w_mean[1]*dt/2.0) + (w_mean[2]*dt/2.0)*(w_mean[0]*dt/2.0),
-         (w_mean[2]*dt/2.0) - (w_mean[0]*dt/2.0)*(w_mean[1]*dt/2.0));
-
-  q = p*q;
-  q.normalize();               // new unit quaternion = previous position q + local integration p
-
-  quat p1( 1, w[0]/2*dt/10, w[1]/2*dt/10, w[2]/2*dt/10); // quaternion for the instantaneous position change
-  quat q1 = p1*q;
-  q1.normalize();       // unit quaternion to represent the next position after a tiny timestep forward
-
-  // extract Euler angles (currently in 1,2,3)
+  // extract Euler angles after integrating with mean angular velocity
+  q = integrate_quat_aleksi(q, (w + w0) / 2.0, dt);
+  q.normalize();
   orient = q.euler();
 
-  // extract Euler angles after small timestep (currently in 1,2,3)
+  // extract Euler angles after small timestep
+  float dt_small = dt/10;
+  quat q1 = integrate_quat_fast(q, w, dt_small);
+  q1.normalize();
+
+  // approximate instantaneous Euler velocities
   euler_angle e1 = q1.euler();
-  dorient.phi   = (e1.phi   - orient.phi)/(dt/10);
-  dorient.theta = (e1.theta - orient.theta)/(dt/10);
-  dorient.psi   = (e1.psi   - orient.psi)/(dt/10); // instantaneous Euler velocities
+  dorient.phi   = (e1.phi   - orient.phi)/dt_small;
+  dorient.theta = (e1.theta - orient.theta)/dt_small;
+  dorient.psi   = (e1.psi   - orient.psi)/dt_small;
 
   // save speeds for next call
   w0 = w;
